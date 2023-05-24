4ee7c6b85026e08f8e6eceaddc1bf339d8f2d966
voolyvex <1joeknapp@gmail.com>
2023-05-24 13:41:29 -0500

    Add project files.

---
+++ issue-tracker-api/Models/Issue.cs
@@ -0,0 +1,14 @@
+﻿namespace issue_tracker_api.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum Priority
+    {
+
+    }
+}
