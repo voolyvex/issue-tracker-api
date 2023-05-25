49f489458f16ee1fa0d0abbd457ae8de4f7096e1
voolyvex <1joeknapp@gmail.com>
2023-05-24 22:59:09 -0500

    feat: Created tracker-client console app for displaying responses from the api

---
+++ tracker-client/IssueDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tracker_client
+{
+    internal class IssueDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
---
+++ tracker-client/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using tracker_client;
+
+HttpClient client = new();
+client.BaseAddress = new Uri("https://localhost:7061");
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+HttpResponseMessage response = await client.GetAsync("api/issue");
+response.EnsureSuccessStatusCode();
+
+if (response.IsSuccessStatusCode)
+{
+    var issues = await response.Content.ReadFromJsonAsync<IEnumerable<IssueDto>>();
+    foreach (var issue in issues)
+    {
+        Console.WriteLine($"Title: {issue.Title}");
+        Console.WriteLine($"Description: {issue.Description}");
+    }
+}
+else
+{
+    Console.WriteLine("No results");
+}
+
+Console.ReadLine();
