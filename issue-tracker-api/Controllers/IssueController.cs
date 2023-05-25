bddbdea3f5d097b3f3cedf2d960393e31cb0211e
voolyvex <1joeknapp@gmail.com>
2023-05-24 22:14:22 -0500

    Created GetById, Post, and Put endpoints in IssueController

--- issue-tracker-api/Controllers/IssueController.cs
+++ issue-tracker-api/Controllers/IssueController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using issue_tracker_api.Data;
 using issue_tracker_api.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Reflection.Metadata.Ecma335;
 
 namespace issue_tracker_api.Controllers
 {
@@ -16,7 +18,40 @@ namespace issue_tracker_api.Controllers
 
         [HttpGet]
         public async Task<IEnumerable<Issue>> Get()
-            => await _context.Issues.ToListAsync();
+        {
+            return await _context.Issues.ToListAsync();
+        }
+
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Issue issue)
+        {
+            if (id != issue.Id) return BadRequest();
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
 
     }
 }
