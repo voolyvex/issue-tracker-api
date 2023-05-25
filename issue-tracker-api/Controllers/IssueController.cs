e6f8419fc289af4413101aec73c335d30874a596
voolyvex <1joeknapp@gmail.com>
2023-05-24 19:29:50 -0500

    Created the SQL Server database and migrated the models + Created GET endpoint in IssueController.cs

---
+++ issue-tracker-api/Controllers/IssueController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using issue_tracker_api.Data;
+using issue_tracker_api.Models;
+
+namespace issue_tracker_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get()
+            => await _context.Issues.ToListAsync();
+
+    }
+}
